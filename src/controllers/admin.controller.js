f25e6ea4ff2e8eeb50e9591a970070f3ebf1834a
karann09 <karankumar011kp@gmail.com>
2024-10-07 10:15:51 +0530

    added routes and controller for admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -24,4 +24,66 @@ const addBikeInfo = asyncHandler(async(req, res)=>{
 
 })
 
-export {addBikeInfo}
+const updateBikeInfo = asyncHandler(async(req, res)=> {
+    const {brand, bikeName, category} = req.body;
+
+    const {id} = req.params;
+
+    const updateFields = {};
+
+    if (brand !== undefined) {
+        updateFields.brand = brand;
+    }
+    if (bikeName !== undefined) {
+        updateFields.bikeName = bikeName;
+    }
+    if (category !== undefined) {
+        updateFields.category = category;
+    }
+
+    if(Object.keys(updateFields).length === 0){
+        throw new ApiError(400, "Please Provide atleast one field to update")
+    }
+
+    const updateBike = await BikeInfo.findByIdAndUpdate(id, updateFields,
+        {new: true}
+    )
+
+    if(!updateBike){
+        throw new ApiError(404, "Bike not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updateBike, "Bike info updated"))
+})
+
+const deleteBikeInfo = asyncHandler(async(req, res)=>{
+
+    const {id} = req.params;
+
+    const deleteBike = await BikeInfo.findByIdAndDelete(id);
+
+    if(!deleteBike){
+        throw new ApiError(404, "Bike not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, "Bike Info deleted successfully"))
+})
+
+const getAllBikeInfo = asyncHandler(async(req, res)=>{
+    const bikes = await BikeInfo.find({});
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, bikes, "Bike Info retrieved"))
+})
+
+export {
+    addBikeInfo,
+    updateBikeInfo,
+    deleteBikeInfo,
+    getAllBikeInfo
+}
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { addBikeInfo } from "../controllers/admin.controller.js";
+import { addBikeInfo, deleteBikeInfo, getAllBikeInfo, updateBikeInfo } from "../controllers/admin.controller.js";
 
 const router = express.Router()
 
-router.post("/", addBikeInfo)
+router.post("/add", addBikeInfo);
+
+router.put("/update/:id", updateBikeInfo);
+
+router.delete("/delete/:id", deleteBikeInfo);
+
+router.get("/getall", getAllBikeInfo);
 
 export default router;
